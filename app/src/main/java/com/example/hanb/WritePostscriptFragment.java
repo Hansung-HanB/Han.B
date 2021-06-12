4d1767512abe06123e25f3024826bfcbd30eef7d
LSH <ssongsuni@naver.com>
2021-06-12 13:01:04 +0900

    autocompletetextview 추가

--- app/src/main/java/com/example/hanb/WritePostscriptFragment.java
+++ app/src/main/java/com/example/hanb/WritePostscriptFragment.java
@@ -13,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
@@ -29,10 +31,16 @@ import org.json.JSONObject;
 public class WritePostscriptFragment extends Fragment {
     private View view;
     private Button enrollButton_write_postscript;
-    private SearchView search_write_postscript;
     private ConstraintLayout write_postscript_layout;
     private RatingBar ratingBar_write_postscript;
     private EditText psText_write_postscript;
+    private AutoCompleteTextView search_write_postscript;
+    String[] programs = {"상상빌리지 방중 체력 단련 프로그램", "사진공모전", "총장님과 식사", "지식재산능력시험(IPAT) 대비 문제풀이 특강", "또래상담", "global culture video competition", "한성영어캠프", "english corner",
+            "group speak", "open activity", "영어수필대회", "3D 프린팅 특강", "독서클럽", "한성인 글쓰기 대회", "창업동아리", "낙산학습나눔", "상시진로시스템", "학습능력향상 튜터링", "학습법워크숍", "VR/AR 특강", "global pal & mentoring",
+            "내외국인 재학생 연합봉사단", "종합심리검사", "단기영어연수", "한성토익강좌", "공무원시험특강", "복학생워크숍", "트랙제 적응 향상 기초학습역량 프로그램", "학습전략검사", "한성대학교 캠퍼스타운 사업단 서포터즈", "상상력교양대학 서포터즈",
+            "집단상담프로그램", "한성점프업", "영자신문사", "한성대신문사", "글로벌튜터링", "HS 한성인의 도전 이야기"};
+
+
     //ViewGroup viewGroup;
 
 
@@ -79,7 +87,7 @@ public class WritePostscriptFragment extends Fragment {
 
 
 
-                String program = search_write_postscript.toString();
+                String program = search_write_postscript.getText().toString();
                 String postscript = psText_write_postscript.getText().toString();
                 float ratingbar = ratingBar_write_postscript.getRating();
 
@@ -125,6 +133,13 @@ public class WritePostscriptFragment extends Fragment {
         write_postscript_layout.setOnClickListener(v -> {
             InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             inputMethodManager.hideSoftInputFromWindow(psText_write_postscript.getWindowToken(), 0);
+        });
+
+        search_write_postscript.setOnClickListener(v -> {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_dropdown_item_1line, programs);
+            AutoCompleteTextView actv = (AutoCompleteTextView)search_write_postscript;
+            actv.setAdapter(adapter);
+            actv.showDropDown();
         });
 
         return view;
